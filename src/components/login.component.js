0fccdc4b47bb7b34472fef8d1996b349c2a139b5
naumanshigri <naumanshigri94@gmail.com>
2022-11-05 17:37:45 +0500

    added first commit

---
+++ src/components/login.component.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <form>
+      <h3>Sign In</h3>
+
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+        />
+      </div>
+
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input mx-2"
+            id="customCheck1"
+          />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
+        </div>
+      </div>
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+
+      <p className="forgot-password text-right">
+        Don't have account <Link to="/sign-up">Register ?</Link>
+      </p>
+    </form>
+  );
+};
+export default Login;
